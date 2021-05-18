8c92f2ee6ef9dfc86d3328e39a3c448b71b83c21
IKJYOT-53 <ikjyot.cse1@gmail.com>
2021-05-18 21:35:42 +0530

    first commit

---
+++ program1.c
@@ -0,0 +1,7 @@
+//WAP to find divisor or factorial of a given number
+#include <stdlib.h>
+#include<stdio.h>
+int main(){
+	printf("%s\n", "hello world");
+	return 0;
+}
---
+++ program2.c
@@ -0,0 +1,7 @@
+//WAP to find the sum of a geometric series
+#include <stdlib.h>
+#include<stdio.h>
+int main(){
+	printf("%s\n", "hello world");
+	return 0;
+}
